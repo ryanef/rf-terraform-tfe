2aa9b4e5f57454e5b05e9d64fe97be37e6daab1a
ryan-wsl <ryanef39@gmail.com>
2024-02-24 13:30:08 -0500

    github repo workspace

--- locals.tf
+++ locals.tf
@@ -18,6 +18,7 @@ locals {
     "github-workspace" = {
       description = "Workspace for GH automation"
       execution_mode = "local"
+      organization_name = var.organization_name
       project_id = module.project["ryanlive"].project_id
       vcs_repo_id = "${var.github_org_name}/rf-terraform-gh"
     }
